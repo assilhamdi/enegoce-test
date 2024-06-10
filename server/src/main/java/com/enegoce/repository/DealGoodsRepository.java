54c9fe5dfeb3634b7c989347c106e719d5e88156
Assil <assil.hamdi@esprit.tn>
2024-06-10 17:00:02 +0100

    Update 2.5 (dealparty, dealgoods, Settlement)

--- server/src/main/java/com/enegoce/repository/DealGoodsRepository.java
+++ server/src/main/java/com/enegoce/repository/DealGoodsRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface DealGoodsRepository extends JpaRepository<DealGoods,Integer> {
+public interface DealGoodsRepository extends JpaRepository<DealGoods, Integer> {
 
     @Query("SELECT dg FROM DealGoods dg WHERE dg.deal.id = :dealId")
     List<DealGoods> findGoodsByDealId(@Param("dealId") Integer dealId);
